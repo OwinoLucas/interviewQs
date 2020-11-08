11d86f165034e7f9d5eb5c5c6934809804c97299
Lucas Owino <lucowish35@gmail.com>
2020-11-08 16:09:25 +0300

    add readme

---
+++ question3.py
@@ -0,0 +1,36 @@
+# Zeraki Assessment Questions
+
+# Authors
+- [Lucas Otieno](https: // github.com/OwinoLucas)
+
+# Project setup instructions
+To view and run/test my answers in your local machine use the following commands:
+
+- `git clone https: // github.com/OwinoLucas/interviewQs.git`
+- `cd interviewQs`
+- `atom .`
+- `code .` [//]:  # (this is if Visual Studio Code is your preferred text editor)
+- `Run python3 question1, 2 or 3 in terminal to test the fuctions`
+
+# License info
+MIT License
+
+Copyright(c) 2018 Lucas Owino
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
